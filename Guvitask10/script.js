2119e4b2d6c78a2150bad879892b8261bb900efb
Navaneeth1797 <139314934+Navaneeth1797@users.noreply.github.com>
2023-12-01 03:24:28 +0530

    Add files via upload

---
+++ Guvitask10/script.js
@@ -0,0 +1,58 @@
+setTimeout(() => {
+    
+    document.getElementById("displaycountdown").innerText = "10"
+    
+    setTimeout(() => {
+    
+        document.getElementById("displaycountdown").innerText = "9"
+        
+        setTimeout(() => {
+   
+            document.getElementById("displaycountdown").innerText = "8"
+            
+            setTimeout(() => {
+    
+                document.getElementById("displaycountdown").innerText = "7"
+                
+                setTimeout(() => {
+    
+                    document.getElementById("displaycountdown").innerText = "6"
+                    
+                    setTimeout(() => {
+    
+                        document.getElementById("displaycountdown").innerText = "5"
+                        
+                        setTimeout(() => {
+    
+                            document.getElementById("displaycountdown").innerText = "4"
+                            
+                            setTimeout(() => {
+                                document.getElementById("displaycountdown").innerText = "3"
+                                
+                                setTimeout(() => {
+                                    document.getElementById("displaycountdown").innerText = "2"
+                                    
+                                    setTimeout(() => {
+                                        document.getElementById("displaycountdown").innerText = "1"
+                                        
+                                        setTimeout(() => {
+                                            document.getElementById("displaycountdown").innerText = "HAPPY INDEPENDENCE DAY"
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+
+ 
+
+
+   
+        
+    
