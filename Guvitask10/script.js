f08db332f96a3aabcefc3b825ffff5c70a403766
Navaneeth1797 <139314934+Navaneeth1797@users.noreply.github.com>
2023-12-01 03:16:27 +0530

    Update script.js

--- Guvitask10/script.js
+++ Guvitask10/script.js
@@ -1,37 +1,54 @@
-
-
-function countdown(Number, callback) {
-      
-    if (Number > 0) {
-
-        document.getElementById
-        ('displaycountdown').innerText = Number;
+setTimeout(() => {
+    
+    document.getElementById("displaycountdown").innerText = "10"
+    
+    setTimeout(() => {
+    
+        document.getElementById("displaycountdown").innerText = "9"
         
-        setTimeout(function () {
-           
-            countdown(Number - 1, callback)
+        setTimeout(() => {
+   
+            document.getElementById("displaycountdown").innerText = "8"
             
+            setTimeout(() => {
+    
+                document.getElementById("displaycountdown").innerText = "7"
+                
+                setTimeout(() => {
+    
+                    document.getElementById("displaycountdown").innerText = "6"
+                    
+                    setTimeout(() => {
+    
+                        document.getElementById("displaycountdown").innerText = "5"
+                        
+                        setTimeout(() => {
+    
+                            document.getElementById("displaycountdown").innerText = "4"
+                            
+                            setTimeout(() => {
+                                document.getElementById("displaycountdown").innerText = "3"
+                                
+                                setTimeout(() => {
+                                    document.getElementById("displaycountdown").innerText = "2"
+                                    
+                                    setTimeout(() => {
+                                        document.getElementById("displaycountdown").innerText = "1"
+                                        
+                                        setTimeout(() => {
+                                            document.getElementById("displaycountdown").innerText = "HAPPY INDEPENDENCE DAY"
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
         }, 1000);
-        
-    }
-    
-    else {
-        
-        callback();
-        
-      }
-    
-}
+    }, 1000);
+}, 1000);
 
-function display() {
-
-    document.getElementById('displaycountdown').innerText = "HAPPY INDEPENDENCE DAY";
-
-}
-
-countdown(10, function(){
-    display();
-});
  
 
 
